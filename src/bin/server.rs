b7976fe0d90c21cd51d663a5d746be510c457f83
ardhika23 <ardhika@gmail.com>
2024-05-07 15:43:20 +0700

    Experiment 2.2: Modifying port.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -35,8 +35,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let (bcast_tx, _) = channel(16);
     let clients: HashMap<SocketAddr, Sender<String>> = HashMap::new(); // Removed `mut` from here
 
-    let listener = TcpListener::bind("127.0.0.1:2000").await?;
-    println!("Listening on port 2000");
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Listening on port 8080");
 
     while let Ok((socket, addr)) = listener.accept().await {
         println!("New connection from {}", addr);
